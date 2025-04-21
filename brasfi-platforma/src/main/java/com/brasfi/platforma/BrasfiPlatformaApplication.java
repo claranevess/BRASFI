02799a8b43482bb15521f2f783e0a66c94defe66
biacosta7 <contact.beatrizcosta@gmail.com>
2025-04-21 15:26:59 -0300

    initial

---
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/BrasfiPlatformaApplication.java
@@ -0,0 +1,13 @@
+package com.brasfi.platforma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasfiPlatformaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasfiPlatformaApplication.class, args);
+	}
+
+}
---
+++ brasfi-platforma/src/test/java/com/brasfi/platforma/BrasfiPlatformaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brasfi.platforma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasfiPlatformaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
