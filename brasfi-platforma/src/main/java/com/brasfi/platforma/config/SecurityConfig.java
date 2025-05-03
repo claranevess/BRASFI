008f0a9b1ae3a7123be4208c77b3cf08f3861f20
biacosta7 <contact.beatrizcosta@gmail.com>
2025-05-03 12:46:44 -0300

    logout e resolução dos bugs de auth

--- brasfi-platforma/src/main/java/com/brasfi/platforma/config/SecurityConfig.java
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.brasfi.platforma.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,17 +20,21 @@ public class SecurityConfig {
                         .anyRequest().authenticated() // tudo o que não foi listado acima também exige login
                 )
                 .formLogin(login -> login
-                        .loginPage("/login").permitAll()
+                        .loginPage("/login") // especifica a página de login
+                        .permitAll() // permite que todos acessem a página de login
                 )
-
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll // libera acesso à tela de login para todos
+                .logout(logout -> logout
+                        .logoutUrl("/logout")                     // URL chamada para logout
+                        .logoutSuccessUrl("/login?logout")        // para onde redirecionar após logout
+                        .invalidateHttpSession(true)              // invalida a sessão
+                        .deleteCookies("JSESSIONID")              // limpa o cookie de sessão
+                        .permitAll()
                 )
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/h2-console/**")
+                        .ignoringRequestMatchers("/h2-console/**") // CSRF desabilitado para h2-console
                 )
                 .headers(headers -> headers
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable
-                        )
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable) // permite iframe para h2-console
                 );
 
         return http.build();
@@ -39,7 +42,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); //  encriptar senhas
     }
-
 }
