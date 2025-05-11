e862ed829bd10ba88114c6558311220e879991ab
biacosta7 <contact.beatrizcosta@gmail.com>
2025-05-11 07:08:30 -0300

    criação da classe Post

---
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/model/Post.java
@@ -0,0 +1,31 @@
+package com.brasfi.platforma.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User autor;
+
+    private LocalDate dataCriacao;
+
+    // TO-DO: adicionar o atributo de comentarios
+
+    // TO-DO: adicionar o atributo de curtidas
+}
