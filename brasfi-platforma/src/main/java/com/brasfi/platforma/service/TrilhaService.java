a4835a82177db1b85dd73408dc4472134b0a1892
Sofia1653 <sofedu2005@gmail.com>
2025-05-12 18:04:42 -0300

    tirando a classe vídeo

--- brasfi-platforma/src/main/java/com/brasfi/platforma/service/TrilhaService.java
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/service/TrilhaService.java
@@ -1,9 +1,7 @@
 package com.brasfi.platforma.service;
 
 import com.brasfi.platforma.model.Trilha;
-import com.brasfi.platforma.model.Video;
 import com.brasfi.platforma.repository.TrilhaRepository;
-import com.brasfi.platforma.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
