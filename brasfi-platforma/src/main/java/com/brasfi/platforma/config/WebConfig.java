ae8e2974783cdab57eb739c22d060024cfad9928
gabsvelozo <gabsmastellari@gmail.com>
2025-06-03 08:11:20 -0300

    html detalheTrilha estático

--- brasfi-platforma/src/main/java/com/brasfi/platforma/config/WebConfig.java
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/config/WebConfig.java
@@ -1,15 +1,15 @@
-package com.brasfi.platforma.config;
+    package com.brasfi.platforma.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+    import org.springframework.context.annotation.Configuration;
+    import org.springframework.web.servlet.config.annotation.*;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Expõe arquivos da pasta "uploads/" no sistema de arquivos para URLs acessíveis via /uploads/**
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/"); // Caminho relativo ao diretório do projeto
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            // Expõe arquivos da pasta "uploads/" no sistema de arquivos para URLs acessíveis via /uploads/**
+            registry.addResourceHandler("/uploads/**")
+                    .addResourceLocations("file:uploads/"); // Caminho relativo ao diretório do projeto
+        }
     }
-}
