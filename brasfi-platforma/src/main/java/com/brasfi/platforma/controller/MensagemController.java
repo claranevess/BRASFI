99bffd0cba355bb65bcfbbd5618cfb69b12785ab
Sofia1653 <sofedu2005@gmail.com>
2025-06-03 20:54:11 -0300

    controller basico para o chat

--- brasfi-platforma/src/main/java/com/brasfi/platforma/controller/MensagemController.java
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/controller/MensagemController.java
@@ -1,15 +1,15 @@
 package com.brasfi.platforma.controller;
 
-import com.brasfi.platforma.model.Mensagem;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/chat")
 public class MensagemController {
-    public Mensagem sendMessage(Mensagem message){
-        return message;
+
+    @GetMapping
+    public String exibirChat() {
+        return "mensagem";
     }
 }
