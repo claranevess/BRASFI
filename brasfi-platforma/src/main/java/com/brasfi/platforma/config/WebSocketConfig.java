9516835b880a7206388038be1aa47df54585afd0
Sofia1653 <sofedu2005@gmail.com>
2025-06-03 20:44:47 -0300

    front de deletar pronto

--- brasfi-platforma/src/main/java/com/brasfi/platforma/config/WebSocketConfig.java
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/config/WebSocketConfig.java
@@ -17,7 +17,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/chat");
+        registry.setApplicationDestinationPrefixes("/add");
         registry.enableSimpleBroker("/canal");
     }
 }
--- brasfi-platforma/src/main/java/com/brasfi/platforma/controller/MensagemController.java
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/controller/MensagemController.java
@@ -4,12 +4,11 @@ import com.brasfi.platforma.model.Mensagem;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/chat")
 public class MensagemController {
-
-    @MessageMapping("/chatMessage")
-    @SendTo("/canal")
     public Mensagem sendMessage(Mensagem message){
         return message;
     }
