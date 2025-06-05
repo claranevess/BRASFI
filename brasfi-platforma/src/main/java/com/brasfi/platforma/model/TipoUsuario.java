91ff482b9f88dc98ca769fd17acc7cb29a50fc07
gabsvelozo <gabsmastellari@gmail.com>
2025-06-04 23:09:55 -0300

    displayValue para tipoUsuario

--- brasfi-platforma/src/main/java/com/brasfi/platforma/model/TipoUsuario.java
+++ brasfi-platforma/src/main/java/com/brasfi/platforma/model/TipoUsuario.java
@@ -1,6 +1,16 @@
 package com.brasfi.platforma.model;
 
 public enum TipoUsuario {
-    ADMINISTRADOR,
-    ESTUDANTE
+    ADMINISTRADOR("Administrador"),
+    ESTUDANTE("Estudante");
+
+    private final String displayValue;
+
+    TipoUsuario(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
